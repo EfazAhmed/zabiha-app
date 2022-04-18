f1a2187684a80cfdafa2ea4d202ba87cc93fbc1f
EfazAhmed <efazahmededu@gmail.com>
2022-04-18 02:20:00 -0400

    Finished search feature

--- components/Search.js
+++ components/Search.js
@@ -1,30 +1,34 @@
-import { Dimensions, StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import { Dimensions, Keyboard, StyleSheet, SafeAreaView, Text, View, TouchableHighlight } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { useState } from 'react';
 import { POSITIONSTACK_APIKEY, POSITIONSTACK_BASE_URL } from '../config';
 
 function Search() {
 
-    const [userSearch, setUserSearch] = useState('')
     const screenWidth = Dimensions.get('screen').width
-    const screenHeight = Dimensions.get('screen').height
-    const [test, setTest] = useState('testing')
-    const [location, setLocation] = useState(null)
+    const [userSearch, setUserSearch] = useState('')
+    const [locations, setLocations] = useState(null)
+    const [selected, setSelected] = useState(null)
+    
 
     const updateSearch = (search) => {
-        const url = `${POSITIONSTACK_BASE_URL}forward?access_key=${POSITIONSTACK_APIKEY}&query=${userSearch}`
-
-        setUserSearch(search);
+        if(search.length >= 2) {
+            setUserSearch(search);
+            getLocations()
+            console.log(locations)
+        } else {
+            setLocations(null)
+            setUserSearch(null);
+        }
     };
 
-    const submitSearch = (search) => {
-        const url = `${POSITIONSTACK_BASE_URL}forward?access_key=${POSITIONSTACK_APIKEY}&query=${userSearch}`
+    const getLocations = () => {
+        const url = `${POSITIONSTACK_BASE_URL}forward?access_key=${POSITIONSTACK_APIKEY}&query=${userSearch}&limit=5&country=US&region=NYC`
         fetch(url)
         .then((res) => res.json())
         .then(
             (result) => {
-                console.log(result)
-                setLocation(result)
+                setLocations(result.data)
             }
         )
         .catch((error) => {
@@ -44,12 +48,51 @@ function Search() {
                     containerStyle={{width: screenWidth, backgroundColor: 'white'}}
                     inputContainerStyle={{backgroundColor: 'white', height: 30}}
                     inputStyle={{height:20, fontSize: 18}}
-                    onSubmitEditing={submitSearch}
                 />
+                {locations && (
+                    <View>
+                        {locations.map((item, index) => (
+                            <TouchableHighlight onPress = {() => {
+                                    setSelected(item)
+                                    setLocations(null)
+                                    setUserSearch(null)
+                                    Keyboard.dismiss()
+                                }}>
+                                <View style={styles.searchOptionContainer}>
+                                    <Text key={index.toString()} style={styles.searchOptionText}> 
+                                        {item.label}
+                                    </Text>
+                                </View>
+                            </TouchableHighlight>
+                        ))}
+                    </View>
+                )}
+                {!locations && selected && (
+                    <View>
+                        <Text>{selected.label}</Text>
+                        <Text>Longitude: {selected.longitude}</Text>
+                        <Text>Latitude: {selected.latitude}</Text>
+                    </View>
+                )}
+                
             </View>
         </SafeAreaView>
     );
 }
 
+const styles = StyleSheet.create({
+    searchOptionContainer: {
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingVertical: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: 'gray'
+    },
+    searchOptionText: {
+        fontSize: 18,
+        paddingVertical: 5,
+        color: 'gray',
+    },
+})
 
 export default Search;
